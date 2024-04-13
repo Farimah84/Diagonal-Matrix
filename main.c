830ee8acb65e57e846b243308476e922156c8ab7
Farimah Nourpanah <139590346+Farimah84@users.noreply.github.com>
2024-04-13 12:57:41 +0330

    Add files via upload

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Farimah Nourpanah Sunday class 18 p.m
+// Homework C book, chapter 5, number 2
+
+void main ();
+{
+    int n[4][4];
+    for (i = 0; i < 4; i++)
+        for (j = 0; j < 4; j++)
+        printf("%d", &n[i][j]);
+}
